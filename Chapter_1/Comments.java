ef11b546ddf2409067a0a6660e2492e9b92f9997
JohnEmmanuelLenizo-UCM <johnemmanuellenizo99@gmail.com>
2025-08-28 19:25:47 +0800

    added files FavoriteSong.java, FirstDialog.java, Comments.java

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+
+   public static void main(String[] args) {
+      //This is a single-line comment
+      
+      /*This is a block comment or a multi-line comment*/
+      
+      /**
+      This is a JavaDoc comment
+      */
+   }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,14 @@
+public class FavoriteSong {
+   
+   public static void main(String[] args) {
+      String lyrics = """
+      I'm looking down now that it's over
+      Reflecting on all of my mistakes
+      I thought I found the road to somewhere
+      Somewhere in His grace
+      I cried out, "Heaven, save me" (save me)
+      But I'm down to one last breath
+      """;
+      System.out.print(lyrics);
+   }
+}
