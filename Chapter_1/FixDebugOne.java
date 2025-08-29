a698002161dc4266d5a0dda845bba76e7a0ad51c
JohnEmmanuelLenizo-UCM <johnemmanuellenizo99@gmail.com>
2025-08-29 15:37:41 +0800

    update added FixDebugOne file

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,20 @@
+public class FixDebugOne
+{
+/* This program displays a greeting */
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+   }
+}
+/*
+List down the spotted bugs below:
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+Bugs:
+1. wrong syntax, class name should not include whitespace "FixDebug One", and must follow Pascal casing "FixDebugOne"
+2. wrong syntax for method identifier "Main" instead of "main"
+3. wrong syntax for method parameters "String args" instead of "String[] args"
+4. In the statement "Systm.out.println("Hello World!")", contains "Systm" instead of "System"
+5. In the statement "Systm.out.println("Hello World!")", missing semicolon ';' at the end of statement
+6. Missing closing brace '}' for the class
+*/
