a1b08da1ed33b0ce4867a6fd30f8fb70cb572a10
JohnEmmanuelLenizo-UCM <johnemmanuellenizo99@gmail.com>
2025-08-29 18:23:31 +0800

    update

--- Chapter_1/FirstDialog.java
+++ Chapter_1/FirstDialog.java
@@ -3,6 +3,6 @@ import javax.swing.JOptionPane;
 public class FirstDialog {
 
    public static void main(String[] args) {
-      JOptionPane.showMessageDialog(null, "First Java Dialog");
+      JOptionPane.showMessageDialog(null, "First Java dialog");
    }
 }
