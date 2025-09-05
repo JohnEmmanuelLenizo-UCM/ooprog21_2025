0c1231f39a89795090e9682a618e5ed5ab2c2fd9
JohnEmmanuelLenizo-UCM <johnemmanuellenizo99@gmail.com>
2025-09-06 07:26:21 +0800

    added Chapter_2 directory and UsingJOptionPaneDialogs.java

---
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -0,0 +1,28 @@
+import javax.swing.JOptionPane;
+
+public class UsingJOptionPaneDialogs {
+
+   public static void main(String[] a) {
+      int confirmation = 1;
+      String name = "";
+      
+      do {
+         name = JOptionPane.showInputDialog(null, "What is your name?");
+         
+         if (name == null) {
+            JOptionPane.showMessageDialog(null, "Operation Cancelled.");
+            break;
+         }
+         else if (name.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Please enter a valid name.");
+            continue;
+         }  
+         else {
+         confirmation = JOptionPane.showConfirmDialog(null, "Do you want to proceed?", "Confirmation", JOptionPane.YES_NO_OPTION);
+         }
+         
+      } while(confirmation == 1);
+      
+      if (name != null) JOptionPane.showMessageDialog(null, "Hello, " + name + "!");   
+   }
+}
