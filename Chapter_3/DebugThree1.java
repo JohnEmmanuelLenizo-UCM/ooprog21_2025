ae0537413c5fd7edb44a3fa4565d2ad7c417d4f7
JohnEmmanuelLenizo-UCM <johnemmanuellenizo99@gmail.com>
2025-09-19 08:19:37 +0800

    Add files via upload

---
+++ Chapter_3/DebugThree1.java
@@ -0,0 +1,25 @@
+// This class calculates a waitperson's tip
+// as 15% of the bill
+import java.util.Scanner;
+class DebugThree1 //removed access specifier "public" to support older version of java
+{
+   public static void main(String args[])
+   {
+      double check1;
+      double check2;
+      Scanner input = new Scanner(System.in);
+      System.out.print("Enter the amount of your check >> ");
+      check1 = input.nextDouble(); //change to 'input.nextDouble()' instead of 'input.nextInt()' to acccept decimal values
+      System.out.print("Enter the amount of your friend's check >> ");
+      check2 = input.nextDouble(); //error in 'inputnextInt()' changed to 'input.nextDouble()'
+      calcTip(check1);
+      calcTip(check2);
+   }
+   public static void calcTip(double bill) // added 'double bill' parameter 
+   {
+      final double RATE = 0.15; //syntax error 'doubel RATE' changed to 'double RATE'
+      double tip;
+      tip = bill * RATE; //Logic error, incorrect formula use 'bill / RATE', changed to 'bill * RATE'
+      System.out.println("The tip should be at least $" + tip);
+   }
+}
