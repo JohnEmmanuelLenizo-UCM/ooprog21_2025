29144d29e7e11ce349753b93f82b087e078a9bd9
JohnEmmanuelLenizo-UCM <johnemmanuellenizo99@gmail.com>
2025-09-26 09:25:19 +0800

    Update DebugFour1.java

--- Chapter_4/DebugFour1.java
+++ Chapter_4/DebugFour1.java
@@ -26,5 +26,4 @@ public class DebugFour1
  * fixed statement "c = new DebugCircle(c);" into "c = new DebugCircle(radius);" to pass the 'radius' variable as an argument and instantiate the 'c circle object
  * In statement "System.out.println("The radius is " + c.getRad());" changed the method to "c.getRadius" to retrieve the radius value
  * In statement "System.out.println("The diameter is " + c.getDiam());" changed the method to "c.getDiameter()" to retrieve the diameter value
- * Fixed main method
  */
