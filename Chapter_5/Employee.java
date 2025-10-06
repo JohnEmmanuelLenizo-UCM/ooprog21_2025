3d015941f7ea7ac5d0be3f68956f1ad06bb84dd8
JohnEmmanuelLenizo-UCM <johnemmanuellenizo99@gmail.com>
2025-10-06 20:24:36 +0800

    Update Employee.java

--- Chapter_5/Employee.java
+++ Chapter_5/Employee.java
@@ -6,8 +6,24 @@ public class Employee {
     private final double overtimeMultiplier = 1.5;
 
     public Employee (int employeeNumber, double payRate) {
-        this.employeeNumber = employeeNumber;
-        this.payRate = payRate;
+      if (employeeNumber > 0 && employeeNumber <= maxEmployeeNumber) {
+         this.employeeNumber = employeeNumber;
+      }
+      else {
+         System.out.println("Invalid. Employee number must be between 0 and 9999");
+         System.exit(0);
+      }
+      if (payRate > 0 && payRate <= maxRate) {
+         this.payRate = payRate;
+      }
+      else if (payRate > maxRate){
+         System.out.println("Invalid. Maxium pay rate is 60.00");
+         System.exit(0);
+      }
+      else if (payRate <= 0) {
+         System.out.println("Invalid. Pay rate must not be 0 or less");
+         System.exit(0);
+      }
     }
 
     //getters
